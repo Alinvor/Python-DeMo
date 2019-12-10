e91d35158ebba5f3aa7d48ae911166698f373f65
zhenwei-li <zhenwei-li@qq.com>
2019-12-10 13:43:09 +0800

    Demo input 替换为raw input 函数

--- src/Demo.py
+++ src/Demo.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
 if __name__ == "__main__":
-    name = input("What\'s your name?\n")
+    name = raw_input("What\'s your name?\n")
     print("Hello %s" % name)
     raw_input("按任意键结束")
