2e490c176b427031f6c95ffdd889537d9ca03bbe
zhenwei-li <zhenwei-li@qq.com>
2019-12-10 17:12:23 +0800

    打印格式化

--- src/Demo.py
+++ src/Demo.py
@@ -1,7 +1,10 @@
 # -- coding:utf-8 --
 
 if __name__ == "__main__":
+    # print "\n"
+    print "-" * 10 + '>'
     name = raw_input("What\'s your name?\n")
     print("Hello %s" % name)
+    print "." * 10
     raw_input("按任意键结束")
-    print("... 系统执行 %s,此时我要 %s ." % ('结束', '退出'))
+    print("." * 3 + "系统执行 %s,此时我要 %s.\n" % ('结束', '退出'))
