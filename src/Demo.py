1116e0ca1f5983e611918ac61c5b4e618430310c
zhenwei-li <zhenwei-li@qq.com>
2019-12-10 17:19:29 +0800

    打印格式化(高级版本)

--- src/Demo.py
+++ src/Demo.py
@@ -1,10 +1,16 @@
 # -- coding:utf-8 --
 
 if __name__ == "__main__":
+    place_holder = '%s %r'
     # print "\n"
     print "-" * 10 + '>'
     name = raw_input("What\'s your name?\n")
-    print("Hello %s" % name)
+    # 注意print 后面',' 的高级表达方式
+    print "->",
+    # print("Hello %s" % name)
+    # print(place_holder % ("Hello", name))
+    print(place_holder % ("Hello", name)),
+    print "<-"
     print "." * 10
     raw_input("按任意键结束")
     print("." * 3 + "系统执行 %s,此时我要 %s.\n" % ('结束', '退出'))
