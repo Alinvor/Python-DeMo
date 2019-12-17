98860d57cd7675044a5848c2b7c64b6a5697dd55
zhenwei-li <zhenwei-li@qq.com>
2019-12-17 17:30:37 +0800

    测试用例统一改为test 前缀

--- src/标准库/test_argv.py
+++ src/标准库/test_argv.py
@@ -10,12 +10,12 @@ def test_argv():
     # ['start_date=2019', 'end_date=2020']
     # argv = sys.argv[:]
     # 输出格式:
-    # ['\xe6\xb5\x8b\xe8\xaf\x95argv.py', 'start_date=2019', 'end_date=2020']
+    # ['test_argv.py', 'start_date=2019', 'end_date=2020']
     print(argv)
 
 
 if __name__ == "__main__":
     '''主函数入口'''
     # the python command
-    # python 测试argv.py start_date=2019 end_date=2020
+    # python test_argv.py start_date=2019 end_date=2020
     test_argv()
