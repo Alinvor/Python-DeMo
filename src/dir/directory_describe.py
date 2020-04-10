f45287c4f080c252778ff1556e2437e47a082ef1
zhenwei-li <zhenwei-li@qq.com>
2020-04-10 10:59:28 +0800

    文件目录迭代用例

--- src/dir/directory_describe.py
+++ src/dir/directory_describe.py
@@ -2,6 +2,8 @@
 
 import os
 __cwd = None
+__filter = []
+__result = []
 
 
 def get_current_directory():
@@ -15,22 +17,66 @@ def set_current_directory(dir):
     __cwd = dir
 
 
+def get_current_filter():
+    ''' the get current filter '''
+    return __filter
+
+
+def set_current_filter(filter=[]):
+    ''' the set current filter '''
+    global __filter
+    __filter = filter
+
+
+def get_current_result():
+    ''' the get current result '''
+    return __result
+
+
+def clear():
+    ''' the result list is clear '''
+    global __result
+    __result = []
+
+
+def _query(dir_name):
+    clear()
+    for root, dirs, files in os.walk(dir_name):
+        for fileName in files:
+            absPath = os.path.join(root, fileName)
+            get_current_result().append(absPath)
+    return get_current_result()
+
+
+def _queryAndFilter(dir_name, filter=[]):
+    clear()
+    for root, dirs, files in os.walk(dir_name):
+        for fileName in files:
+            absPath = os.path.join(root, fileName)
+            ext = os.path.splitext(absPath)[1]
+            if ext in filter:
+                get_current_result().append(absPath)
+    return get_current_result()
+
+
 def execute():
     ''' the execute tasks '''
     absPath = os.path.abspath(get_current_directory())
     if (os.path.exists(absPath)):
-        recursion(absPath)
+        _query(absPath)
 
 
-def recursion(dest):
-    ''' the recursion file or directory '''
-    if (os.path.isdir(dest)):
-        os.listdir(dest)
-        pass
-    else:
-        pass
+def executeFilter():
+    ''' the execute tasks and filter '''
+    absPath = os.path.abspath(get_current_directory())
+    if (os.path.exists(absPath)):
+        _query(absPath, get_current_filter())
 
 
 if __name__ == "__main__":
     '''主函数入口'''
-    pass
+    # DeMo 实例
+    absDir = '/Users/XXX/Documents/html'
+    set_current_directory(absDir)
+    execute()
+    print('the current dir list is %s' % get_current_result())
