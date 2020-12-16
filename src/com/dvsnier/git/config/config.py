a278cba8c6073af89d4ea04419978402efaceabe
zhenwei-li <zhenwei-li@qq.com>
2020-12-16 14:02:04 +0800

    the git config script

---
+++ src/com/dvsnier/git/config/config.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+from com.dvsnier.git.git import Git
+from com.dvsnier.process.execute import execute
+
+
+class Config(Git, object):
+    'the git config information'
+
+    def __init__(self):
+        super(Config, self).__init__()
+
+    def global_info(self):
+        'the global information'
+        return execute(['git config --global --list'])
+
+    def local_info(self):
+        'the local git information'
+        return execute(['git config --global --list'])
