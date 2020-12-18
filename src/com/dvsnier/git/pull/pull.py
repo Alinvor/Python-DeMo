7efa08e223b1868c275794e56d1617cc51475e19
zhenwei-li <zhenwei-li@qq.com>
2020-12-18 16:46:19 +0800

    the git pull script

---
+++ src/com/dvsnier/git/pull/pull.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+from com.dvsnier.git.git import Git
+from com.dvsnier.process.execute import execute
+
+
+class Pull(Git, object):
+    'the git pull command class'
+
+    def __init__(self):
+        super(Pull, self).__init__()
+
+    def fast_foward(self):
+        'the execute git pull origin option'
+        return execute(['git pull origin'])
