72601ada4b541f135857e2936755b7fe3880e1f5
zhenwei-li <zhenwei-li@qq.com>
2021-06-08 17:17:06 +0800

    the branch test suite upgrade

--- tests/com/dvsnier/git/branch/test_branch.py
+++ tests/com/dvsnier/git/branch/test_branch.py
@@ -1,50 +1,57 @@
 # -*- coding:utf-8 -*-
 
-from com.dvsnier.git.branch.branch import Branch
+import logging
 import unittest
+
+from com.dvsnier.git.branch.branch import Branch
+from com.dvsnier.git.git import Git
 
 
 class Test_Branch(unittest.TestCase):
     ''' the test branch '''
+
+    _git = None
+
     @classmethod
     def setUpClass(cls):
         print("...the set up...")
-        print
+        print('')
+        cls._git = Git()
+        cls._git.config()
 
     def setUp(self):
-        return super(Test_Branch, self).setUp()
+        super(Test_Branch, self).setUp()
 
     def test_get_branch(self):
         branch = Branch()
         branch_name = branch.get_branch()
-        # print(branch_name)
+        logging.info(branch_name)
         self.assertIsNotNone(branch_name, 'test_get_branch is error.')
-        print "the test get_branch is succeed."
+        print("the test get_branch is succeed.")
 
     def test_branch_to_file_and_commit_list(self):
         branch = Branch()
         branch_commit_list = branch.branch_to_file_and_commit_list()
-        # print(branch_commit_list)
+        logging.info(branch_commit_list)
         self.assertIsNotNone(branch_commit_list,
                              'test_branch_to_file_and_commit_list is error.')
-        print "the test branch_to_file_and_commit_list is succeed."
+        print("the test branch_to_file_and_commit_list is succeed.")
 
     def test_branch_to_file_and_commit_list_with_more(self):
         branch = Branch()
-        branch_commit_list_more = branch.branch_to_file_and_commit_list_with_more(
-        )
-        # print(branch_commit_list_more)
+        branch_commit_list_more = branch.branch_to_file_and_commit_list_with_more()
+        logging.info(branch_commit_list_more)
         self.assertIsNotNone(
             branch_commit_list_more,
             'test_branch_to_file_and_commit_list_with_more is error.')
-        print "the test branch_to_file_and_commit_list_with_more is succeed."
+        print("the test branch_to_file_and_commit_list_with_more is succeed.")
 
     def tearDown(self):
-        return super(Test_Branch, self).tearDown()
+        super(Test_Branch, self).tearDown()
 
     @classmethod
     def tearDownClass(cls):
-        print
+        print('')
         print("...the tear down...")
 
 
