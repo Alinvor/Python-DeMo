6f8c2a97e4bdbd4bda8c511e8a459aa650de2313
zhenwei-li <zhenwei-li@qq.com>
2021-06-10 18:30:30 +0800

    the checkout test suite

--- tests/com/dvsnier/git/checkout/test_checkout.py
+++ tests/com/dvsnier/git/checkout/test_checkout.py
@@ -1,34 +1,42 @@
 # -*- coding:utf-8 -*-
 
-from com.dvsnier.git.checkout.checkout import Checkout
+import logging
 import unittest
+
+from com.dvsnier.git.branch.branch import Branch
+from com.dvsnier.git.checkout.checkout import Checkout
 
 
 class Test_Checkout(unittest.TestCase):
     ''' the test checkout '''
+
     @classmethod
     def setUpClass(cls):
         print("...the set up...")
-        print
+        print('')
+        cls._branch = Branch()
+        cls._branch.set_current_branch_name(cls._branch.get_branch())
 
     def setUp(self):
         return super(Test_Checkout, self).setUp()
 
     def test_branch_checkout(self):
         branch_checkout = Checkout()
-        branch_checkout.branch_checkout()
+        current_branch = Test_Checkout._branch.get_current_branch_name()
+        logging.debug('the current branch name is {}'.format(current_branch))
+        branch_checkout.branch_checkout(current_branch)
         # branch_checkout.branch_checkout(branch='anonym')
-        print "the test branch_checkout is succeed."
-        pass
+        # print("the test branch_checkout is succeed.")
 
     def tearDown(self):
         return super(Test_Checkout, self).tearDown()
 
     @classmethod
     def tearDownClass(cls):
-        print
+        print('')
         print("...the tear down...")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test_Checkout)
+    unittest.TextTestRunner(verbosity=2).run(suite)
