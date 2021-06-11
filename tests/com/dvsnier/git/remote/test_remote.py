5a10c8ac2c7db033ba1518bb27368e2da8c3f2be
zhenwei-li <zhenwei-li@qq.com>
2021-06-11 10:54:01 +0800

    the git remote test suite

--- tests/com/dvsnier/git/remote/test_remote.py
+++ tests/com/dvsnier/git/remote/test_remote.py
@@ -1,33 +1,38 @@
 # -*- coding:utf-8 -*-
 
-from com.dvsnier.git.remote.remote import Remote
 import unittest
+
+from com.dvsnier.git.git import Git
+from com.dvsnier.git.remote.remote import Remote
 
 
 class Test_Remote(unittest.TestCase):
     ''' the test remote '''
+
     @classmethod
     def setUpClass(cls):
         print("...the set up...")
-        print
+        print('')
+        cls._git = Git()
+        cls._git.config()
+        cls._prune = Remote()
 
     def setUp(self):
         return super(Test_Remote, self).setUp()
 
     def test_branch_prune(self):
-        prune = Remote()
-        prune.branch_prune()
-        print "the test branch_prune is succeed."
-        pass
+        Test_Remote._prune.branch_prune()
+        # print("the test branch_prune is succeed.")
 
     def tearDown(self):
         return super(Test_Remote, self).tearDown()
 
     @classmethod
     def tearDownClass(cls):
-        print
+        print('')
         print("...the tear down...")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test_Remote)
+    unittest.TextTestRunner(verbosity=2).run(suite)
