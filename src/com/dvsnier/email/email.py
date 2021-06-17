cf23d7701b03277420e4c8a4442cf58e849bd942
zhenwei-li <zhenwei-li@qq.com>
2021-06-17 12:05:06 +0800

    添加 email 用例

---
+++ src/com/dvsnier/email/email.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+
+from com.dvsnier.email.config.config import Config
+
+
+class Email(object):
+    '''the email class'''
+
+    # the config object
+    _config = None
+    # the xxx.cfg config dict information
+    _cfg = {}
+
+    def __init__(self):
+        super(Email, self).__init__()
+        self._config = Config()
+
+    def template(self):
+        ''' the template method '''
+        pass
+
+    # def get_config(self):
+    #     ''' the get config information '''
+    #     return self._config
+
+    # def set_config(self, config):
+    #     ''' the set config information '''
+    #     if not config:
+    #         self._config = config
+
+    def config_file(self, config_file):
+        ''' the read xxx.cfg '''
+        # logging.debug('the current config file is {}'.format(config_file))
+        self._cfg = self._config.obtain_config(config_file)
+        return self
+
+    def get_config_info(self):
+        ''' the get config information '''
+        return self._cfg
+
+    def init(self):
+        ''' the initlizated email environment '''
+        pass
---
+++ tests/com/dvsnier/email/test_email.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+import logging
+import unittest
+
+from com.dvsnier.config.common_conf import logging_conf
+from com.dvsnier.email.email import Email
+
+
+class Test_Email(unittest.TestCase):
+    ''' the test email '''
+
+    @classmethod
+    def setUpClass(cls):
+        print("...the set up...")
+        print('')
+        kwargs = {'output_dir_name': 'email', 'file_name': 'email'}
+        logging_conf(kwargs)
+        cls._email = Email()
+
+    def setUp(self):
+        super(Test_Email, self).setUp()
+
+    def test_0_config_file(self):
+        self._email.config_file('conf/email_config.test.cfg')
+
+    def test_1_get_config_info(self):
+        cfg_info = self._email.get_config_info()
+        self.assertIsNotNone(cfg_info)
+        logging.info(cfg_info)
+        # print(cfg_info)
+
+
+    def tearDown(self):
+        super(Test_Email, self).tearDown()
+
+    @classmethod
+    def tearDownClass(cls):
+        print('')
+        print("...the tear down...")
+
+
+if __name__ == '__main__':
+    ''' the unittest suite '''
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test_Email)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    # unittest.main()
