9b3a04eb9052da0ded5a3a6c3fcc76fc3b842e0d
zhenwei-li <zhenwei-li@qq.com>
2021-06-17 10:16:45 +0800

    添加邮件配置项

--- src/com/dvsnier/email/config/config.py
+++ src/com/dvsnier/email/config/config.py
@@ -9,6 +9,19 @@ class Config(object):
     '''the email config'''
 
     _sep = "\\" if sys.platform == "win32" else "/"  # no os module here yet - poor mans version
+    # the Set up server
+    _mail_host = None
+    # the Server SSL default port
+    _mail_port = None
+    # the email user name
+    _mail_user = None
+    # the email Password
+    _mail_pass = None
+    # the email Sender
+    _mail_sender = None
+    # the email Receiver
+    _mail_receiver = None
+
 
     def __init__(self):
         super(Config, self).__init__()
@@ -30,4 +43,56 @@ class Config(object):
                 config[line[:split_at].strip()] = line[split_at + 1:].strip()
         return config
 
+    def get_mail_host(self):
+        ''' the get mail host '''
+        return self._mail_host
 
+    def set_mail_host(self, mail_host):
+        ''' the set mail host '''
+        self._mail_host = mail_host
+        return self
+
+    def get_mail_port(self):
+        ''' the get mail port '''
+        return self._mail_port
+
+    def set_mail_port(self, mail_port):
+        ''' the set mail port '''
+        self._mail_port = mail_port
+        return self
+
+    def get_mail_user(self):
+        ''' the get mail user '''
+        return self._mail_user
+
+    def set_mail_user(self, mail_user):
+        ''' the set mail user '''
+        self._mail_user = mail_user
+        return self
+
+    def get_mail_pass(self):
+        ''' the get mail password '''
+        return self._mail_pass
+
+    def set_mail_pass(self, mail_pass):
+        ''' the set mail password '''
+        self._mail_pass = mail_pass
+        return self
+
+    def get_mail_sender(self):
+        ''' the get mail sender '''
+        return self._mail_sender
+
+    def set_mail_sender(self, mail_sender):
+        ''' the set mail sender '''
+        self._mail_sender = mail_sender
+        return self
+
+    def get_mail_receiver(self):
+        ''' the get mail receiver '''
+        return self._mail_receiver
+
+    def set_mail_receiver(self, mail_receiver):
+        ''' the set mail receiver '''
+        self._mail_receiver = mail_receiver
+        return self
