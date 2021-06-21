01a8549623ae2ef5ed3d4bc9ed9172419a9383f1
zhenwei-li <zhenwei-li@qq.com>
2021-06-21 17:02:03 +0800

    添加 callback 模块

---
+++ src/com/dvsnier/email/callback/icallback.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+
+class ICallback(object):
+    ''' the callback interface class '''
+
+    def __init__(self):
+        super(ICallback, self).__init__()
+
+    def callback(self):
+        pass
