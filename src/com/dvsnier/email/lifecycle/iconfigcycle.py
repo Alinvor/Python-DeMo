b334bfac737c1f6a809b1c499ba52e9d1667caf6
zhenwei-li <zhenwei-li@qq.com>
2021-06-21 13:43:24 +0800

    添加 config 属性

--- src/com/dvsnier/email/lifecycle/iconfigcycle.py
+++ src/com/dvsnier/email/lifecycle/iconfigcycle.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
 
+from typing import Union
+from com.dvsnier.email.config.config import Config
 from com.dvsnier.email.lifecycle.icycle import ICycle
 
 
 class IConfigCycle(ICycle, object):
     ''' the config cycle class '''
+
+    # the config instance
+    _config: Union[Config, None]
 
     def __init__(self):
         super(IConfigCycle, self).__init__()
@@ -16,3 +21,12 @@ class IConfigCycle(ICycle, object):
     def onExecute(self):
         ''' the dynamic code information execution mapping  '''
         pass
+
+    def get_config(self):
+        ''' the get config information '''
+        return self._config
+
+    def set_config(self, config):
+        ''' the set config information '''
+        self._config = config
+        return self
