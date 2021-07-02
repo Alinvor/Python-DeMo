7b93e6a14d6c18379045a5190ef4456fcdfb985e
zhenwei-li <zhenwei-li@qq.com>
2021-07-02 10:49:18 +0800

    添加拆分 conf 模块

--- src/com/dvsnier/config/common_conf.py
+++ src/com/dvsnier/config/common_conf.py
@@ -1,27 +1,16 @@
 # -*- coding:utf-8 -*-
 
 import logging
+from com.dvsnier.config.common_config import config
+from deprecated import deprecated
 
-from com.dvsnier.directory.common_dir import generate_complex_file_name
 
-
+@deprecated(version='0.0.2.dev1',
+            reason="You should use the com.dvsnier.common_config.config method,\
+    that We will delete this method after extending 2-3 versions")
 def logging_conf(kwargs):
     '''
         the logging conf info:
-            logging_conf(**kwargs={output_dir_name=\' \', file_name=\' \', level=logging.DEBUG})
+            logging_conf(**kwargs={output_dir_name=\' \', file_name=\' \', level=logging.ERROR})
     '''
-    if kwargs.get('output_dir_name') is None or len(kwargs.get('output_dir_name')) == 0:
-        raise KeyError('the current kwargs[output_dir_name] is empty.')
-    if kwargs.get('file_name') is None or len(kwargs.get('file_name')) == 0:
-        raise KeyError('the current kwargs[file_name] is empty.')
-    if kwargs.get('level') < 0:
-        raise KeyError('the current kwargs[level] is invalid.')
-    file_name = generate_complex_file_name(kwargs['output_dir_name'], kwargs['file_name'])
-    logging.basicConfig(
-        filename=file_name,
-        filemode='a',
-        format='[%(asctime)s][%(levelname)8s] --- %(message)s',
-        # format=
-        # '[%(asctime)s][%(levelname)8s][%(filename)s:%(lineno)s] --- %(message)s',
-        level=kwargs['level'])
-    logging.info('this current file is %s' % (file_name))
+    return config(kwargs)
---
+++ src/com/dvsnier/config/common_config.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from com.dvsnier.directory.common_dir import generate_complex_file_name
+
+
+def config(kwargs):
+    '''
+        the logging config info:
+            config(**kwargs={output_dir_name=\' \', file_name=\' \', level=logging.ERROR})
+    '''
+    if kwargs.get('output_dir_name') is None or len(kwargs.get('output_dir_name')) == 0:
+        raise KeyError('the current kwargs[output_dir_name] is empty.')
+    if kwargs.get('file_name') is None or len(kwargs.get('file_name')) == 0:
+        raise KeyError('the current kwargs[file_name] is empty.')
+    if kwargs.get('level') is not None and kwargs.get('level') < 0:
+        raise KeyError('the current kwargs[level] is invalid.')
+    else:
+        kwargs['level'] = logging.ERROR
+    file_name = generate_complex_file_name(kwargs['output_dir_name'], kwargs['file_name'])
+    logging.basicConfig(
+        filename=file_name,
+        filemode='a',
+        format='[%(asctime)s][%(levelname)8s] --- %(message)s',
+        # format=
+        # '[%(asctime)s][%(levelname)8s][%(filename)s:%(lineno)s] --- %(message)s',
+        level=kwargs['level'])
+    logging.info('this current file is %s' % (file_name))
+    return logging
