9b689f439a52bbb0900d40b98b863b541361c9b4
zhenwei-li <zhenwei-li@qq.com>
2021-07-02 17:13:55 +0800

    适配 python >= 3.3 同命名空间，不同包名加载问题

--- src/com/dvsnier/config/common_conf.py
+++ src/com/dvsnier/config/common_conf.py
@@ -5,8 +5,8 @@ from deprecated import deprecated
 
 
 @deprecated(version='0.0.2.dev1',
-            reason="You should use the com.dvsnier.common_config.config method,\
-    that We will delete this method after extending 2-3 versions")
+            reason="You should use the from com.dvsnier.config.common_config import config method, that We will delete \
+this method after extending 2-3 versions")
 def logging_conf(kwargs):
     '''
         the logging conf info:
