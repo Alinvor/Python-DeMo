4189d9b608f31a209dd31f5d9e6743ff07287a15
zhenwei-li <zhenwei-li@qq.com>
2021-07-03 15:06:11 +0800

    添加 config 测试用例

--- src/com/dvsnier/config/common_config.py
+++ src/com/dvsnier/config/common_config.py
@@ -8,16 +8,35 @@ from com.dvsnier.directory.common_dir import generate_complex_file_name
 def config(kwargs):
     '''
         the logging config info:
-            config(**kwargs={output_dir_name=\' \', file_name=\' \', level=logging.ERROR})
+            config(**kwargs={output_dir_name=\' \', file_name=\' \', level=logging.WARNING})
+
+        the level value range:
+            - CRITICAL = 50
+            - FATAL = CRITICAL
+            - ERROR = 40
+            - WARNING = 30
+            - WARN = WARNING
+            - INFO = 20
+            - DEBUG = 10
+            - NOTSET = 0
+
+        the article link reference:
+
+            1. https://docs.python.org/zh-cn/2.7/howto/logging.html#logging-basic-tutorial
+            2. https://docs.python.org/zh-cn/2.7/howto/logging.html#logging-advanced-tutorial
+            3. https://docs.python.org/zh-cn/2.7/howto/logging-cookbook.html#logging-cookbook
+            4. https://docs.python.org/zh-cn/2.7/library/logging.html
     '''
     if kwargs.get('output_dir_name') is None or len(kwargs.get('output_dir_name').strip()) == 0:
         raise KeyError('the current kwargs[output_dir_name] is empty.')
     if kwargs.get('file_name') is None or len(kwargs.get('file_name').strip()) == 0:
         raise KeyError('the current kwargs[file_name] is empty.')
-    if kwargs.get('level') is not None and kwargs.get('level') < 0:
+    if kwargs.get('level') is not None and kwargs.get('level') < logging.NOTSET:
         raise KeyError('the current kwargs[level] is invalid.')
+    elif kwargs.get('level') is None or kwargs.get('level') == logging.NOTSET:
+        kwargs['level'] = logging.WARNING
     else:
-        kwargs['level'] = logging.ERROR
+        pass
     file_name = generate_complex_file_name(kwargs['output_dir_name'], kwargs['file_name'])
     logging.basicConfig(
         filename=file_name,
