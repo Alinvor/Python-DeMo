42f96fe76aed7167310e87d829d210835689d4f1
zhenwei-li <zhenwei-li@qq.com>
2021-07-07 12:58:45 +0800

    the rm cache command upgrade

--- .rmcache.bash
+++ .rmcache.bash
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-## THE DELETE FILES
+### THE DELETE FILES
 find . -type f -name '*.pyc' -delete
 
-## THE DELETE DIRECTORY
-# .tox/*
+### THE DELETE DIRECTORY
+## .tox/*
 find .tox -path "*" -delete
-# ./build/*
+## ./build/*
 find ./build -path "*" ! -name "build" -delete
-# ./src/com.dvsnier.*.egg-info
+## ./src/com.dvsnier.*.egg-info
 find ./src -path "*com.dvsnier*" -delete
 # find ./src -path "*com.dvsnier.*" -type d -exec rm -r {} +
 # find ./src -path "*com.dvsnier.*" -type f -delete
-# ./dist/*
+## ./dist/*
 find ./dist -path "*" ! -name "dist" -delete
-# ./out/log/*
+## ./out/log/*
 find ./out/log -path "*" ! -name "log" -delete
-# __pycache__
+## __pycache__
 find . -type d -name __pycache__ -exec rm -r {} +
--- .rmcache.ps1
+++ .rmcache.ps1
@@ -4,19 +4,21 @@
 #   1. https://docs.microsoft.com/zh-cn/powershell/scripting/samples/working-with-files-and-folders?view=powershell-7.1
 #   2. https://docs.microsoft.com/zh-cn/powershell/scripting/samples/working-with-registry-keys?view=powershell-7.1
 #
-## THE DELETE FILES
+
+### THE DELETE FILES
 Get-ChildItem -Path . -include *.pyc -Recurse | Remove-Item
-## THE DELETE DIRECTORY
-# .tox/*
+
+### THE DELETE DIRECTORY
+## .tox/*
 Remove-Item -Path .\.tox -Recurse
-# ./build/*
+## ./build/*
 Remove-Item -Path .\build\* -Recurse
-# ./src/com.dvsnier.*.egg-info
+## ./src/com.dvsnier.*.egg-info
 Remove-Item -Path .\src\com.dvsnier.*.egg-info -Recurse
 # Get-ChildItem -Path .\src\com.dvsnier.*.egg-info -Recurse
-# ./dist/*
+## ./dist/*
 Remove-Item -Path .\dist\* -Recurse
-# ./out/log/*
+## ./out/log/*
 Remove-Item -Path .\out\log\* -Recurse
-# __pycache__
+## __pycache__
 Get-ChildItem -Path . -include __pycache__ -Recurse | Remove-Item
