e34010f31ccd51c16e39503b29ca0a46cf3d392c
zhenwei-li <zhenwei-li@qq.com>
2021-07-08 13:01:53 +0800

    Popen 适配 Python 2.7 规范

--- src/com/dvsnier/process/execute.py
+++ src/com/dvsnier/process/execute.py
@@ -13,34 +13,33 @@ def execute(cmds, quiet=True):
     content = ''
     stdouts = None
     start = time.time()
-    with subprocess.Popen(cmds[0], stdout=subprocess.PIPE, shell=True) as p:
-        # logging.debug('the current sub process pid(cwd: %s, ppid: %s, id: %s%d).' %
+    p = subprocess.Popen(cmds[0], stdout=subprocess.PIPE, shell=True)
+    # logging.debug('the current sub process pid(cwd: %s, ppid: %s, id: %s%d).' %
+    #               (p.pid, os.getppid(), type(p), id(p)))
+    processes = [p]
+    for x in cmds[1:]:
+        #
+        # the airticle link reference:
+        #
+        # 1. https://docs.python.org/2.7/library/subprocess.html#subprocess.Popen.communicate
+        # 2. https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
+        # 3. https://stackoverflow.com/questions/58649679/resourcewarning-unclosed-file-io-bufferedreader-name-4
+        # 4. https://python.readthedocs.io/en/stable/library/subprocess.html
+        #
+        p = subprocess.Popen(x, stdin=p.stdout, stdout=subprocess.PIPE, bufsize=1024, shell=True)
+        # logging.debug(
+        #     'the current sub process pid(cwd: %s, ppid: %s, id: %s%d).' %
+        #     (p.pid, os.getppid(), type(p), id(p)))
+        # logging.debug(type(p), id(p))
+        processes.append(p)
+        # logging.debug('the current run process pid(cwd: %s, ppid: %s, id: %s%d).' %
         #               (p.pid, os.getppid(), type(p), id(p)))
-        processes = [p]
-        for x in cmds[1:]:
-            #
-            # the airticle link reference:
-            #
-            # 1. https://docs.python.org/2.7/library/subprocess.html#subprocess.Popen.communicate
-            # 2. https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
-            # 3. https://stackoverflow.com/questions/58649679/resourcewarning-unclosed-file-io-bufferedreader-name-4
-            # 4. https://python.readthedocs.io/en/stable/library/subprocess.html
-            #
-            p = subprocess.Popen(x, stdin=p.stdout, stdout=subprocess.PIPE, bufsize=1024, shell=True)
-            # logging.debug(
-            #     'the current sub process pid(cwd: %s, ppid: %s, id: %s%d).' %
-            #     (p.pid, os.getppid(), type(p), id(p)))
-            # logging.debug(type(p), id(p))
-            processes.append(p)
-            # logging.debug('the current run process pid(cwd: %s, ppid: %s, id: %s%d).' %
-            #               (p.pid, os.getppid(), type(p), id(p)))
-            # logging.debug(type(p), id(p))
-        stdouts, stderrs = p.communicate()
-        if stderrs:
-            logging.error(stderrs)
-        for p in processes:
-            p.kill()
-            p.wait()
+        # logging.debug(type(p), id(p))
+    stdouts, stderrs = p.communicate()
+    if stderrs:
+        logging.error(stderrs)
+    for p in processes:
+        p.wait()
 
     end = time.time()
     if not quiet:
