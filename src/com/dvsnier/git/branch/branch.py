41295a42b13bb24c52b67060f744dcdbaef2d219
zhenwei-li <zhenwei-li@qq.com>
2021-08-03 10:52:01 +0800

    the branch upgrade

--- src/com/dvsnier/git/branch/branch.py
+++ src/com/dvsnier/git/branch/branch.py
@@ -16,6 +16,7 @@ class Branch(Git, object):
 
     def get_branch(self):
         'the get current branch name'
+        current_branch = None
         current_branch_list_str = execute(["git branch"])
         if len(current_branch_list_str) > 0:
             branch_name_list = ' '.join(str(current_branch_list_str).split())
@@ -24,12 +25,13 @@ class Branch(Git, object):
                 for index in range(len(branch_s)):
                     if branch_s[index] == '*':
                         current_branch = branch_s[index + 1]
-                        return current_branch
+                        break
             else:
                 logging.warn('the current branch name list with re is vaild.')
             pass
         else:
             logging.warn('the current branch name is empty.')
+        return current_branch
 
     def branch_to_file_and_commit_list(self):
         'the execute origin branch commit record list that to files'
@@ -59,9 +61,7 @@ class Branch(Git, object):
         if not branch_name or not len(branch_name.strip()) > 0:
             logging.exception('the current branch name is invalid.')
             raise KeyError('the current branch name is invalid.')
-        return execute([
-            'git branch -u origin/{0} {0}'.format(branch_name)
-        ])
+        return execute(['git branch -u origin/{0} {0}'.format(branch_name)])
 
     def get_current_branch_name(self):
         ''' the obtain current branch name '''
@@ -73,3 +73,57 @@ class Branch(Git, object):
             logging.exception('the current branch name is invalid.')
             raise KeyError('the current branch name is invalid.')
         self._current_branch_name = branch_name
+
+    def get_current_branch_list(self):
+        'the current branch list'
+        branch_queue = []
+        branch_list_strs = execute(['git branch --list']).strip()
+        if branch_list_strs:
+            branch_list = branch_list_strs.split('\n')
+            if branch_list:
+                for branch in branch_list:
+                    branch_element = branch.split(' ')
+                    if branch_element:
+                        branch_queue.append(branch_element[-1])
+        return branch_queue
+
+    def get_remote_branch_list(self):
+        'the current remote branch list'
+        branch_queue = []
+        branch_list_strs = execute(['git branch --remotes']).strip()
+        if branch_list_strs:
+            branch_list = branch_list_strs.split('\n')
+            if branch_list:
+                for branch in branch_list:
+                    branch_element = branch.split(' ')
+                    if branch_element:
+                        branch_queue.append(branch_element[-1])
+        return branch_queue
+
+    def get_remote_prune(self):
+        'the remote prune'
+        prune_result = execute(['git remote prune origin'])
+        return prune_result
+
+    def has_remote(self):
+        'the judge whether the current git repository is associated with the remote repository'
+        local_result = execute(['git config --local --list'])
+        if local_result and local_result.find('remote.origin.url') >= 0 and local_result.find(
+                'remote.origin.fetch') >= 0:
+            return True
+        return False
+
+    def has_specifical_branch(self, branch_name, is_remote=False):
+        'the judge whether the current git repository is specifical branch name'
+        branch_list = []
+        if is_remote:
+            branch_list = self.get_remote_branch_list()
+            if branch_name:
+                branch_name = 'origin/' + branch_name
+            else:
+                raise KeyError('the current branch name is an invalid parameter value')
+        else:
+            branch_list = self.get_current_branch_list()
+        if branch_name and branch_list and len(branch_list) > 0 and branch_name in branch_list:
+            return True
+        return False
--- tests/com/dvsnier/git/branch/test_branch.py
+++ tests/com/dvsnier/git/branch/test_branch.py
@@ -3,20 +3,19 @@
 import logging
 import unittest
 
+from com.dvsnier.config.journal.common_config import config
 from com.dvsnier.git.branch.branch import Branch
 from com.dvsnier.git.git import Git
 
 
 class Test_Branch(unittest.TestCase):
     ''' the test branch '''
-
-    _git = None
-    _branch = None
-
     @classmethod
     def setUpClass(cls):
         print("...the set up...")
         print('')
+        kwargs = {'output_dir_name': 'git', 'file_name': 'log', 'level': logging.DEBUG}
+        config(kwargs)
         cls._git = Git()
         cls._git.config()
         cls._branch = Branch()
@@ -25,55 +24,77 @@ class Test_Branch(unittest.TestCase):
         super(Test_Branch, self).setUp()
 
     def test_0_get_branch(self):
-        branch_name = Test_Branch._branch.get_branch()
+        branch_name = self._branch.get_branch()
         logging.info('the current branch name is {}'.format(branch_name))
         self.assertIsNotNone(branch_name, 'test_0_get_branch is error.')
         # print("the test get_branch is succeed.")
 
     def test_1_branch_to_file_and_commit_list(self):
-        branch_commit_list = Test_Branch._branch.branch_to_file_and_commit_list()
+        branch_commit_list = self._branch.branch_to_file_and_commit_list()
         logging.info(branch_commit_list)
-        self.assertIsNotNone(branch_commit_list,
-                             'test_0_branch_to_file_and_commit_list is error.')
+        self.assertIsNotNone(branch_commit_list, 'test_0_branch_to_file_and_commit_list is error.')
         # print("the test branch_to_file_and_commit_list is succeed.")
 
     def test_2_branch_to_file_and_commit_list_with_more(self):
-        branch_commit_list_more = Test_Branch._branch.branch_to_file_and_commit_list_with_more()
+        branch_commit_list_more = self._branch.branch_to_file_and_commit_list_with_more()
         logging.info(branch_commit_list_more)
-        self.assertIsNotNone(
-            branch_commit_list_more,
-            'test_0_branch_to_file_and_commit_list_with_more is error.')
+        self.assertIsNotNone(branch_commit_list_more, 'test_0_branch_to_file_and_commit_list_with_more is error.')
         # print("the test branch_to_file_and_commit_list_with_more is succeed.")
 
     def test_3_branch_set_upstream_to(self):
-        branch_name = Test_Branch._branch.get_branch()
+        branch_name = self._branch.get_branch()
         # branch_name = None
         # branch_name = ''
         # branch_name = ' '
         # branch_name = '0'
-        branch_set_upstream_to = Test_Branch._branch.branch_set_upstream_to(branch_name)
+        branch_set_upstream_to = self._branch.branch_set_upstream_to(branch_name)
         if branch_set_upstream_to:
             logging.info(branch_set_upstream_to)
         self.assertIsNotNone(branch_set_upstream_to, 'test_3_branch_set_upstream_to is error.')
 
     def test_5_get_current_branch_name(self):
-        # branch_name = Test_Branch._branch.get_branch()
+        # branch_name = self._branch.get_branch()
         # branch_name = None
         # branch_name = ''
         # branch_name = ' '
         # branch_name = '0'
-        current_branch_name = Test_Branch._branch.get_current_branch_name()
+        current_branch_name = self._branch.get_current_branch_name()
         if current_branch_name:
             logging.info('the current branch name is {0}.'.format(current_branch_name))
         self.assertIsNotNone(current_branch_name, 'test_5_get_current_branch_name is error.')
 
     def test_4_set_current_branch_name(self):
-        branch_name = Test_Branch._branch.get_branch()
+        branch_name = self._branch.get_branch()
         # branch_name = None
         # branch_name = ''
         # branch_name = ' '
         # branch_name = '0'
-        Test_Branch._branch.set_current_branch_name(branch_name)
+        self._branch.set_current_branch_name(branch_name)
+
+    def test_get_current_branch_list(self):
+        branch_list = self._branch.get_current_branch_list()
+        self.assertIsNotNone(branch_list, 'test_get_current_branch_list is error.')
+        logging.info(branch_list)
+
+    def test_get_remote_branch_list(self):
+        branch_list = self._branch.get_remote_branch_list()
+        self.assertIsNotNone(branch_list, 'test_get_remote_branch_list is error.')
+        logging.info(branch_list)
+
+    def test_get_remote_prune(self):
+        prune_result = self._branch.get_remote_prune()
+        self.assertIsNotNone(prune_result, 'test_get_remote_prune is error.')
+        logging.info(prune_result)
+
+    def test_has_remote(self):
+        hasRemote = self._branch.has_remote()
+        self.assertTrue(hasRemote, 'test_has_remote is error.')
+        logging.info(hasRemote)
+
+    def test_has_specifical_branch(self):
+        value = self._branch.has_specifical_branch('master', is_remote=True)
+        self.assertTrue(value, 'test_has_specifical_branch is error.')
+        logging.info(value)
 
     def tearDown(self):
         super(Test_Branch, self).tearDown()
